68769b47397bd318a2ecdffa994409c3bd2f1be4
Anders Gustavsson <anders@kreditz.com>
2018-02-13 15:42:29 +0100

    Created folder images and add comments in gosuspace.rb

--- gosuspace.rb
+++ gosuspace.rb
@@ -8,13 +8,15 @@ class Tutorial < Gosu::Window
 		super 800, 600 # How big window game is: 640 px wide & 480px high
 		self.caption = "The Badass Spacegame" # Puts title on the pop window
 		# Fetch and show background image in game window
-		@background_image = Gosu::Image.new("media/space.png", :tileable => true)
+		@background_image = Gosu::Image.new("images/space.png", :tileable => true)
 	end
 	#updates the game when playing
 	def update
 	end
 	# Rendrer game on to gamewindow
 	def draw
+		# Calls instance variable and draws it, the numbers its location where in the gamewindow shall space image be drawn
+		@background_image.draw(0, 0, 0)
 	end
 end
 
